1b558bc63500d45c4a9a2d9f5f366440af1e425c
Antelio I. Abe <antelio@gmail.com>
2016-10-31 20:14:02 -0200

    Add New Job Positon Feature

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -3,4 +3,14 @@ class JobController < ApplicationController
     id = params[:id]
     @job = Job.find(id)
   end
+
+  def new
+    @new_job = Job.new
+  end
+
+  def create
+    job_param = params.require(:job).permit(:title, :company, :category, :description, :location)
+    job = Job.create(job_param)
+    redirect_to job
+  end
 end
